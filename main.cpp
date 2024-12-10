c61d7361c58039a7747aa81e1a87eafeb263d82a
Kitz2024 <kit.pollinger@gmail.com>
2024-12-09 19:05:18 -0800

    Coffee Booth Struct Completed

--- main.cpp
+++ main.cpp
@@ -23,8 +23,19 @@ struct Node {
 //Struct for other Vendors
 struct customerBooth {
     string name;
-    string order;
+    string drinkOrder;
 };
+
+//Function to add new customer to Linked List
+void enqueue(Node*& head, Node*& tail, const string& name, const string& order){
+    Node* newNode = new Node(name, order);
+    if (!tail){
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
 
 //Function to serve to a customer
 void dequeue(Node*& head, Node*& tail){
@@ -38,15 +49,38 @@ void dequeue(Node*& head, Node*& tail){
 
 }
 //Coffee Booth
-void CoffeeBooth(Node*& head, Node*& tail, const vector<string>& names, const vector<string>& orders){
+void CoffeeBooth(Node*& head, Node*& tail, const vector<string>& names, const vector<string>& order){
     if (head){
         cout << "Serving Coffee to: " << head->name << " - " << head->drinkOrder << "\n";
         dequeue(head,tail);
+    } else {
+        cout << "Coffee booth empty. \n";
+    }
+
+    // 50% probability new Customer
+    if(rand() % 2 == 0){
+        int customerIndex = rand() % names.size();
+        int customerOrder = rand() % order.size();
+        cout << "New Customer Joined Coffee Booth: " << names[customerIndex] << " - " << order[customerOrder] << "\n";
     }
 }
 
 int main (){
-    
+    srand(static_cast<unsigned int>(time(0)));
 
+    //Customer Names
+    vector<string> names = {"Ryan", "Harry", "Lisa", "Elizabeth", "Max"};
+    vector<string> coffeeOrders = {"Latte", "Espresso", "Cappuccino"};
+
+    //Coffee booth Linked List
+    Node* coffeeHead = nullptr;
+    Node* coffeeTail = nullptr;
+
+    //Coffee (Linked list)
+    for (int i = 0; i < 3; i++){
+        int customerIndex = rand() % names.size();
+        int customerOrder = rand() % names.size();
+        enqueue(coffeeHead, coffeeTail, names[customerIndex], coffeeOrders[customerOrder]);
+    }
     return 0;
 }
